2e4b100658f13bd763726f388fb4d3639f53028a
Wolfran Pinzon <wolfran2008@gmail.com>
2022-01-12 07:48:25 -0500

    version inicial

---
+++ app/src/main/java/com/wolfran/appceiba/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.wolfran.appceiba
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
